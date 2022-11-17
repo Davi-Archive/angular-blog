1d22bd174e92f466aab48ff9f70b239553fd5959
d1av <davi4alves@gmail.com>
2022-11-17 16:50:52 -0300

    feat: add navbar component and logo

--- src/app/shared/components/layout/navbar/navbar.component.ts
+++ src/app/shared/components/layout/navbar/navbar.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public isTheNavbarCollapsed: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public colapseOrShowNavbar(): string {
+    if (this.isTheNavbarCollapsed) {
+      this.isTheNavbarCollapsed = false;
+      return '';
+    }
+    this.isTheNavbarCollapsed = true;
+    return 'collapse'
+
   }
 
 }
